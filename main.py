2e19203350509bbed9be6d647ba0f43439173715
Baron <nm6101080@gs.ncku.edu.tw>
2021-11-03 13:05:18 +0800

    read file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from spacy.training import Example
 import random
@@ -5,4 +6,11 @@ from spacy.util import minibatch
 import spacy
 from spacy.pipeline import textcat
 
-movie = pd.read_csv("../data")
+train_data = list()
+for f in os.listdir("../data/aclImdb/train/pos/"):
+    file = open("../data/aclImdb/train/pos/{f}", 'r')
+    data = [f.read()]
+
+    train_data.append(zip(data, "pos"))
+    print(train_data)
+    file.close()
